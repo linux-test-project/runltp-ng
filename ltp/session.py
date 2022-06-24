5bb0856612bfae8980fe9408eb1e05ed54550ad8
Andrea Cervesato <andrea.cervesato@suse.com>
2022-06-24 16:17:54 +0200

    Add Session implementation
    Session is the first part interfacing with main.py which is going to be
    implemented after.

---
+++ ltp/session.py
@@ -0,0 +1,294 @@
+"""
+.. module:: session
+    :platform: Linux
+    :synopsis: module that contains LTP session definition
+
+.. moduleauthor:: Andrea Cervesato <andrea.cervesato@suse.com>
+"""
+import os
+import time
+import logging
+import threading
+from ltp import LTPException
+from ltp.sut import SUT
+from ltp.sut import IOBuffer
+from ltp.qemu import QemuSUT
+from ltp.host import HostSUT
+from ltp.results import SuiteResults
+from ltp.events import EventHandler
+from ltp.tempfile import TempRotator
+from ltp.dispatcher import SerialDispatcher
+from ltp.export import JSONExporter
+
+
+class SessionError(LTPException):
+    """
+    Raised when a new exception occurs during session.
+    """
+
+
+class Printer(IOBuffer):
+    """
+    Redirect data from stdout to events.
+    """
+
+    def __init__(self, sut: SUT, events: EventHandler) -> None:
+        self._events = events
+        self._sut = sut
+
+    def write(self, data: bytes) -> None:
+        self._events.fire("run_cmd_stdout", self._sut.name, data)
+
+    def flush(self) -> None:
+        pass
+
+
+class Session:
+    """
+    The main session handler.
+    """
+
+    def __init__(
+            self,
+            events: EventHandler,
+            suite_timeout: float = 3600,
+            exec_timeout: float = 3600) -> None:
+        """
+        :param events: generic event handler
+        :type events: EventHandler
+        :param suite_timeout: timeout before stopping testing suite
+        :type suite_timeout: float
+        :param exec_timeout: timeout before stopping single execution
+        :type exec_timeout: float
+        """
+        self._logger = logging.getLogger("ltp.session")
+        self._events = events
+        self._suite_timeout = max(suite_timeout, 0)
+        self._exec_timeout = max(exec_timeout, 0)
+        self._sut = None
+        self._dispatcher = None
+        self._lock_run = threading.Lock()
+
+        if not events:
+            raise ValueError("events is empty")
+
+    @staticmethod
+    def _setup_debug_log(tmpdir: str) -> None:
+        """
+        Save a log file with debugging information
+        """
+        logger = logging.getLogger()
+        logger.setLevel(logging.DEBUG)
+
+        debug_file = os.path.join(tmpdir, "debug.log")
+        handler = logging.FileHandler(debug_file, encoding="utf8")
+        handler.setLevel(logging.DEBUG)
+
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s:%(lineno)s - %(levelname)s - %(message)s")
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    def _print_results(self, suite_results: SuiteResults) -> None:
+        """
+        Print suite results.
+        """
+        tests = len(suite_results.tests_results)
+
+        self._logger.info("")
+        self._logger.info("Suite name: %s", suite_results.suite.name)
+        self._logger.info("Total Run: %d", tests)
+        self._logger.info("Elapsed time: %.1f s", suite_results.exec_time)
+        self._logger.info("Total Passed Tests: %d", suite_results.passed)
+        self._logger.info("Total Failed Tests: %d", suite_results.failed)
+        self._logger.info("Total Skipped Tests: %d", suite_results.skipped)
+        self._logger.info("Total Broken Tests: %d", suite_results.broken)
+        self._logger.info("Total Warnings: %d", suite_results.warnings)
+        self._logger.info("Kernel Version: %s", suite_results.kernel)
+        self._logger.info("Machine Architecture: %s", suite_results.arch)
+        self._logger.info("Distro: %s", suite_results.distro)
+        self._logger.info("Distro version: %s", suite_results.distro_ver)
+        self._logger.info("")
+
+    def _start_sut(self, ltpdir: str, tmpdir: str, sut_config: dict) -> SUT:
+        """
+        Start a new SUT and return it initialized.
+        """
+        sut_name = sut_config.get("name", None)
+        if sut_name not in ["qemu", "host"]:
+            raise ValueError(f"{sut_name} is not supported")
+
+        testcases = os.path.join(ltpdir, "testcases", "bin")
+
+        env = {}
+        env["PATH"] = "/sbin:/usr/sbin:/usr/local/sbin:" + \
+            f"/root/bin:/usr/local/bin:/usr/bin:/bin:{testcases}"
+
+        sut = None
+        timeout = 0.0
+        if sut_name == 'qemu':
+            config = {}
+            config['env'] = env
+            config['cwd'] = testcases
+            config['tmpdir'] = tmpdir
+            config.update(sut_config)
+
+            sut = QemuSUT(**config)
+            timeout = 360.0
+        else:
+            sut = HostSUT(cwd=ltpdir, env=env)
+            timeout = 10.0
+
+        self._events.fire("sut_start", sut.name)
+
+        sut.communicate(
+            timeout=timeout,
+            iobuffer=Printer(sut, self._events))
+
+        return sut
+
+    def _stop_sut(self, timeout: float = 30) -> None:
+        """
+        Stop a specific SUT.
+        """
+        if not self._sut:
+            return
+
+        self._events.fire("sut_stop", self._sut.name)
+
+        if self._sut.is_running:
+            self._sut.stop(
+                timeout=timeout,
+                iobuffer=Printer(self._sut, self._events))
+        else:
+            self._sut.force_stop(
+                timeout=timeout,
+                iobuffer=Printer(self._sut, self._events))
+
+        self._sut = None
+
+    def _stop_all(self, timeout: float = 30) -> None:
+        """
+        Stop both sut and dispatcher.
+        """
+        if not self._sut:
+            return
+
+        if self._dispatcher:
+            self._dispatcher.stop(timeout=timeout)
+
+        self._stop_sut(timeout=timeout)
+
+    def stop(self, timeout: float = 30):
+        """
+        Stop the current running session.
+        """
+        if not self._sut:
+            return
+
+        self._logger.info("Stopping session")
+
+        self._stop_all(timeout=timeout)
+        self._events.fire("session_stopped")
+
+        t_start = time.time()
+        while self._lock_run.locked():
+            time.sleep(0.05)
+            if time.time() - t_start >= timeout:
+                raise SessionError("Timeout when stopping session")
+
+        self._logger.info("Session stopped")
+
+    # pylint: disable=too-many-locals
+    # pylint: disable=too-many-statements
+    def run_single(
+            self,
+            sut_config: dict,
+            report_path: str,
+            suites: list,
+            command: str,
+            ltpdir: str,
+            tmpdir: str) -> None:
+        """
+        Run some testing suites with a specific SUT configurations.
+        :param sut_config: system under test configuration.
+        :type sut_config: dict
+        :param report_path: path of the report file. If None, it won't be saved
+        :type report_path: None | str
+        :param suites: suites to execute
+        :type suites: list
+        :param command: command to execute
+        :type command: str
+        :param ltpdir: ltp install folder
+        :type ltpdir: str
+        :param tmpdir: temporary directory
+        :type tmpdir: str
+        """
+        if not sut_config:
+            raise ValueError("sut configuration can't be empty")
+
+        with self._lock_run:
+            session_tmpdir = TempRotator(tmpdir).rotate()
+
+            self._logger.info(
+                "Running session using temporary folder: %s",
+                session_tmpdir)
+
+            self._setup_debug_log(session_tmpdir)
+            self._events.fire("session_started", session_tmpdir)
+
+            self._sut = None
+            self._dispatcher = None
+
+            try:
+                self._sut = self._start_sut(ltpdir, session_tmpdir, sut_config)
+
+                self._logger.info("Created SUT: %s", self._sut.name)
+
+                if command:
+                    self._events.fire("run_cmd_start", command)
+
+                    ret = self._sut.run_command(
+                        command,
+                        timeout=self._exec_timeout,
+                        iobuffer=Printer(self._sut, self._events))
+
+                    self._events.fire(
+                        "run_cmd_stop",
+                        command,
+                        ret["returncode"])
+
+                results = None
+                if suites:
+                    self._dispatcher = SerialDispatcher(
+                        ltpdir=ltpdir,
+                        tmpdir=tmpdir,
+                        sut=self._sut,
+                        events=self._events,
+                        suite_timeout=self._suite_timeout,
+                        test_timeout=self._exec_timeout)
+
+                    self._logger.info("Created dispatcher")
+
+                    results = self._dispatcher.exec_suites(suites)
+                    self._dispatcher.stop()
+
+                    if results:
+                        for result in results:
+                            self._print_results(result)
+
+                        if report_path:
+                            exporter = JSONExporter()
+                            exporter.save_file(results, report_path)
+
+                if not suites or (results and len(suites) == len(results)):
+                    # if the number of suites is the same of the number
+                    # of results we can say that session has not been stopped
+                    self._stop_sut(timeout=60)
+                    self._events.fire("session_completed", results)
+                    self._logger.info("Session completed")
+            except LTPException as err:
+                self._stop_all(timeout=60)
+
+                self._logger.error("Error: %s", str(err))
+                self._events.fire("session_error", str(err))
---
+++ ltp/tests/test_session.py
@@ -0,0 +1,295 @@
+"""
+Unittests for session module.
+"""
+import os
+import stat
+import threading
+import pytest
+from ltp.session import Session
+from ltp.events import EventHandler
+from ltp.events import SyncEventHandler
+
+
+class EventsTracer:
+    """
+    Trace events and check they all have been called.
+    """
+    def __init__(
+        self,
+        events: EventHandler,
+        tmpdir: str,
+        sut_name: str,
+        command: str) -> None:
+        self._counter = -1
+        self._messages = []
+        self._tmpdir = tmpdir
+        self._sut_name = sut_name
+        self._command = command
+
+        events.register("session_started", self._session_started)
+        events.register("session_completed", self._session_completed)
+        events.register("session_stopped", self._session_stopped)
+        events.register("session_error", self._session_error)
+        events.register("sut_start", self._sut_start)
+        events.register("sut_stop", self._sut_stop)
+        events.register("run_cmd_start", self._run_cmd_start)
+        events.register("run_cmd_stop", self._run_cmd_stop)
+
+    def next_event(self) -> str:
+        self._counter += 1
+        return self._messages[self._counter]
+
+    def _session_started(self, tmpdir) -> None:
+        assert tmpdir.startswith(self._tmpdir)
+        self._messages.append("session_started")
+
+    def _session_completed(self, results) -> None:
+        self._messages.append("session_completed")
+
+    def _session_stopped(self) -> None:
+        self._messages.append("session_stopped")
+
+    def _session_error(self, err) -> None:
+        assert err is not None
+        self._messages.append("session_error")
+
+    def _sut_start(self, sut_name) -> None:
+        assert sut_name == self._sut_name
+        self._messages.append("sut_start")
+
+    def _sut_stop(self, sut_name) -> None:
+        assert sut_name == self._sut_name
+        self._messages.append("sut_stop")
+
+    def _run_cmd_start(self, command) -> None:
+        assert command == self._command
+        self._messages.append("run_cmd_start")
+
+    def _run_cmd_stop(self, command, retcode) -> None:
+        assert command == self._command
+        assert retcode == 0
+        self._messages.append("run_cmd_stop")
+
+class _TestSession:
+    """
+    Tests for Session implementation.
+    """
+
+    @pytest.fixture
+    def ltpdir(self):
+        """
+        LTP install directory.
+        """
+        raise NotImplementedError()
+
+    @pytest.fixture
+    def suites(self):
+        """
+        LTP suites to run.
+        """
+        raise NotImplementedError()
+
+    @pytest.fixture
+    def sut_config(self):
+        """
+        SUT configuration to implement.
+        """
+        raise NotImplementedError()
+
+    @pytest.fixture
+    def prepare_tmpdir(self, tmpdir):
+        """
+        Prepare the temporary directory adding suites and tests.
+        """
+        # create testcases folder
+        ltpdir = tmpdir.mkdir("ltp")
+        testcases = ltpdir.mkdir("testcases").mkdir("bin")
+
+        script_sh = testcases.join("script.sh")
+        script_sh.write(
+            '#!/bin/bash\n'
+            'echo ""\n'
+            'echo ""\n'
+            'echo "Summary:"\n'
+            'echo "passed   $1"\n'
+            'echo "failed   $2"\n'
+            'echo "broken   $3"\n'
+            'echo "skipped  $4"\n'
+            'echo "warnings $5"\n'
+        )
+
+        st = os.stat(str(script_sh))
+        os.chmod(str(script_sh), st.st_mode | stat.S_IEXEC)
+
+        # create runtest folder
+        runtest = ltpdir.mkdir("runtest")
+
+        suitefile = runtest.join("dirsuite0")
+        suitefile.write("dir01 script.sh 1 0 0 0 0")
+
+        suitefile = runtest.join("dirsuite1")
+        suitefile.write("dir02 script.sh 0 1 0 0 0")
+
+        suitefile = runtest.join("dirsuite2")
+        suitefile.write("dir03 script.sh 0 0 0 1 0")
+
+        suitefile = runtest.join("dirsuite3")
+        suitefile.write("dir04 script.sh 0 0 1 0 0")
+
+        suitefile = runtest.join("dirsuite4")
+        suitefile.write("dir05 script.sh 0 0 0 0 1")
+
+        # create scenario_groups folder
+        scenario_dir = ltpdir.mkdir("scenario_groups")
+
+        scenario_def = scenario_dir.join("default")
+        scenario_def.write("dirsuite0\ndirsuite1")
+
+        scenario_def = scenario_dir.join("network")
+        scenario_def.write("dirsuite2\ndirsuite3\ndirsuite4\ndirsuite5")
+
+    @pytest.mark.usefixtures("prepare_tmpdir")
+    @pytest.mark.parametrize("use_report", [True, False])
+    @pytest.mark.parametrize("command", [None, "ls -1"])
+    def test_run_single(
+            self,
+            tmpdir,
+            use_report,
+            suites,
+            command,
+            sut_config,
+            ltpdir):
+        """
+        Run a session using a specific sut configuration.
+        """
+        report_path = None
+        if use_report:
+            report_path = str(tmpdir / "report.json")
+
+        events = SyncEventHandler()
+        tracer = EventsTracer(
+            events,
+            str(tmpdir),
+            sut_config["name"],
+            command)
+
+        try:
+            session = Session(events)
+            session.run_single(
+                sut_config,
+                report_path,
+                suites,
+                command,
+                ltpdir,
+                str(tmpdir))
+
+            assert tracer.next_event() == "session_started"
+            assert tracer.next_event() == "sut_start"
+
+            if command:
+                assert tracer.next_event() == "run_cmd_start"
+                assert tracer.next_event() == "run_cmd_stop"
+
+            assert tracer.next_event() == "sut_stop"
+
+            if suites:
+                if use_report:
+                    assert os.path.isfile(report_path)
+            else:
+                if use_report:
+                    assert not os.path.exists(report_path)
+
+            assert tracer.next_event() == "session_completed"
+        finally:
+            session.stop()
+
+    @pytest.mark.usefixtures("prepare_tmpdir")
+    def test_stop(self, tmpdir, sut_config, ltpdir, suites):
+        """
+        Run a session using a specific sut configuration.
+        """
+        report_path = str(tmpdir / "report.json")
+
+        events = SyncEventHandler()
+        session = Session(events)
+
+        def _threaded():
+            session.stop(timeout=3)
+
+        thread = threading.Thread(target=_threaded, daemon=True)
+        def stop_exec_suites(test):
+            thread.start()
+
+        events.register("test_started", stop_exec_suites)
+
+        tracer = EventsTracer(
+            events,
+            str(tmpdir),
+            sut_config["name"],
+            None)
+
+        session.run_single(
+            sut_config,
+            report_path,
+            suites,
+            None,
+            ltpdir,
+            str(tmpdir))
+
+        thread.join(timeout=10)
+
+        assert os.path.exists(report_path)
+        assert tracer.next_event() == "session_started"
+        assert tracer.next_event() == "sut_start"
+        assert tracer.next_event() == "sut_stop"
+        assert tracer.next_event() == "session_stopped"
+
+class TestHostSession(_TestSession):
+    """
+    Test Session using host SUT.
+    """
+
+    @pytest.fixture
+    def ltpdir(self, tmpdir):
+        return str(tmpdir / "ltp")
+
+    @pytest.fixture
+    def suites(self):
+        return ["dirsuite0", "dirsuite1"]
+
+    @pytest.fixture
+    def sut_config(self):
+        config = {"name": "host"}
+        return config
+
+
+TEST_QEMU_IMAGE = os.environ.get("TEST_QEMU_IMAGE", None)
+TEST_QEMU_PASSWORD = os.environ.get("TEST_QEMU_PASSWORD", None)
+
+
+@pytest.mark.skipif(TEST_QEMU_IMAGE is None, reason="TEST_QEMU_IMAGE is not defined")
+@pytest.mark.skipif(TEST_QEMU_PASSWORD is None, reason="TEST_QEMU_IMAGE is not defined")
+class TestQemuSession(_TestSession):
+    """
+    Test Session using QemuSUT.
+    """
+
+    @pytest.fixture
+    def ltpdir(self):
+        return "/opt/ltp"
+
+    @pytest.fixture
+    def suites(self):
+        return ["math", "watchqueue"]
+
+    @pytest.fixture
+    def sut_config(self):
+        """
+        Qemu SUT configuration.
+        """
+        config = {
+            "name": "qemu",
+            "image": TEST_QEMU_IMAGE,
+            "password": TEST_QEMU_PASSWORD
+        }
+        return config
