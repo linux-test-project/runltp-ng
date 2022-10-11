9f773edb0bb3c795a6d83032aded198030181ebc
Andrea Cervesato <andrea.cervesato@suse.com>
2022-10-11 18:00:58 +0200

    Test kernel panic in qemu SUT

--- ltp/tests/test_qemu.py
+++ ltp/tests/test_qemu.py
@@ -4,7 +4,9 @@ Test SUT implementations.
 import os
 import pytest
 from ltp.qemu import QemuSUT
+from ltp.sut import KernelPanicError
 from ltp.tests.sut import _TestSUT
+from ltp.tests.sut import Printer
 
 
 TEST_QEMU_IMAGE = os.environ.get("TEST_QEMU_IMAGE", None)
@@ -14,7 +16,28 @@ TEST_QEMU_PASSWORD = os.environ.get("TEST_QEMU_PASSWORD", None)
 @pytest.mark.qemu
 @pytest.mark.skipif(TEST_QEMU_IMAGE is None, reason="TEST_QEMU_IMAGE is not defined")
 @pytest.mark.skipif(TEST_QEMU_PASSWORD is None, reason="TEST_QEMU_IMAGE is not defined")
-class TestQemuSUTISA(_TestSUT):
+class _TestQemuSUT(_TestSUT):
+    """
+    Test Qemu SUT implementation.
+    """
+
+    def test_kernel_panic(self, sut):
+        """
+        Test kernel panic recognition.
+        """
+        try:
+            sut.communicate(iobuffer=Printer())
+
+            with pytest.raises(KernelPanicError):
+                sut.run_command(
+                    "echo 'Kernel panic\nThis is a generic message'",
+                    timeout=10,
+                    iobuffer=Printer())
+        finally:
+            sut.stop(iobuffer=Printer())
+
+
+class TestQemuSUTISA(_TestQemuSUT):
     """
     Test QemuSUT implementation.
     """
@@ -32,10 +55,8 @@ class TestQemuSUTISA(_TestSUT):
         if runner.is_running:
             runner.stop()
 
-@pytest.mark.qemu
-@pytest.mark.skipif(TEST_QEMU_IMAGE is None, reason="TEST_QEMU_IMAGE is not defined")
-@pytest.mark.skipif(TEST_QEMU_PASSWORD is None, reason="TEST_QEMU_IMAGE is not defined")
-class TestQemuSUTVirtIO(_TestSUT):
+
+class TestQemuSUTVirtIO(_TestQemuSUT):
     """
     Test QemuSUT implementation.
     """
